575e188d9e0483c4d5307a0a0b1229668df31325
JacksBergs <Jmtennentethier@gmail.com>
2024-09-19 14:05:11 -0600

    added moving square

---
+++ moving-square/sketch.js
@@ -0,0 +1,11 @@
+// Supposed to have a square but it aint on my puter, 
+//Sept 19, 2024
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
