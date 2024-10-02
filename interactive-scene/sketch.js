ef65d878a563b2647cb63eb67d46b968d7f73ec2
JacksBergs <Jmtennentethier@gmail.com>
2024-10-01 18:23:40 -0600

    update

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -23,31 +23,33 @@ let cooldown = 0;
 
 let speed = 40;
 
-let playerSpeed = 5;
+let playerSpeed = 20;
 
 let patrinc;
 let patX;
 let patY;
 
-let hitTop;
-let hitBot;
+let isBullet = false;
+
+let died;
+
+let deadPlayer = false;
+
+let patSpeed;
 
 function preload(){
   player_sponge = loadImage("Spingle.png");
   gun_1 = loadImage("gun.png");
   bullet_1 = loadImage("bullet.png");
   patrinc = loadImage("Patrink.png");
+  died = loadImage("deades.png");
 }
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   //centering the image
   imageMode(CENTER);
-<<<<<<< HEAD
-  
-=======
   patrickMaker();
->>>>>>> eb5803e41f3ff0f800720553e0b3f9dc130b6c73
 }
 
 function draw() {
@@ -56,16 +58,35 @@ function draw() {
   keyPressed();
   image(player_sponge, x, y, player_sponge.width/5, player_sponge.height/5);
   cooldown++;
-  bulletX += speed;
-  patX -= 0;
-  hitBot = createVector(patX,);
-  hitTop = createVector(patX, patY);
-  if (bulletY >= hitBot.y && bulletY <= hitTop.y){
-    console.log("poo");
+
+  patX -= patSpeed;
+
+  if (patX <= 50){
+    deadPlayer = true;
   }
+
+  if (isBullet){
+    bulletX += speed;
+    image(bullet_1, bulletX, bulletY, bullet_1.width/10, bullet_1.height/10);
+  }
+  if (dist(patX, patY, bulletX, bulletY) <= 50){
+    bulletX = 0;
+    bulletY = 0;
+    cooldown = 30;
+    isBullet = false;
+    patrickMaker();
+  }
+  
   image(gun_1, gun_x, gun_y, gun_1.width/15, gun_1.height/15);
-  image(bullet_1, bulletX, bulletY, bullet_1.width/10, bullet_1.height/10);
   image(patrinc, patX, patY, patrinc.width/5, patrinc.height/5);
+
+  if (deadPlayer){
+    image(died, width/2, height/2, width, height);
+    fill("white");
+    textSize(100);
+    text('You died', width/2, height/2);
+  }
+
 }
 
 function keyPressed(){
@@ -91,15 +112,17 @@ function keyPressed(){
 
 function shoot(){
   if (cooldown >= 30){
-    cooldown = 0;
-    image(bullet_1, bulletX, bulletY, bullet_1.width/10, bullet_1.height/10);
     bulletX = x;
     bulletY = y;
+    isBullet = true;
+    cooldown = 0;
+    image(bullet_1, bulletX, bulletY, bullet_1.width/10, bullet_1.height/10);
   }
 
 }
 
 function patrickMaker(){
+  patSpeed = random(3, 15);
   patX = width;
-  patY = random(200, 300);
+  patY = random(0, height - patrinc.height/5);
 }
