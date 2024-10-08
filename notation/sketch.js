09cb20da40298a8e5adcf2236ab6c5c7da55f95e
JacksBergs <Jmtennentethier@gmail.com>
2024-10-08 14:11:08 -0600

    idea of the project
    It's just an idea for now that I want to bring to fruition

--- notation/sketch.js
+++ notation/sketch.js
@@ -1,5 +1,7 @@
 // New Project
-
+// Jack Tennent-Ethier
+// Input songs from youtube, type in the song and then there will be some 3D art that will bounce according to the decibles of the noise
+// p5 js sound area to look at
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
