e36bf3bf02e90f488c7792d871e83172d58b4cc7
JacksBergs <Jmtennentethier@gmail.com>
2024-10-29 14:12:10 -0600

    starting point

--- grid-proj/sketch.js
+++ grid-proj/sketch.js
@@ -1,9 +1,4 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+//make a moving square game where when blocks are clicked after 6 of them the tiles start to dissapear and you can make a little thing that tries to learn your patterns???
 
 
 function setup() {
